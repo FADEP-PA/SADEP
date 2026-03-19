29b02c49cc908083885c5696f79a8db60696d355
Lucas Madureira <129661036+lucasonline0@users.noreply.github.com>
2026-03-19 10:15:46 -0300

    feat(frontend): add login screen

---
+++ apps/frontend/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'AEP-PA | Login',
+  description: 'Tela de acesso ao sistema AEP-PA.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
